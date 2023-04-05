184da744dc8940550bef2b01d1e464f226c7830d
trisentes <123353826+trisentes@users.noreply.github.com>
2023-04-05 11:19:54 +0700

    Add files via upload

--- perpus.sql
+++ perpus.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2023 at 10:21 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- Waktu pembuatan: 05 Apr 2023 pada 06.19
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `anggota`
+-- Struktur dari tabel `anggota`
 --
 
 CREATE TABLE `anggota` (
@@ -38,16 +38,17 @@ CREATE TABLE `anggota` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `anggota`
+-- Dumping data untuk tabel `anggota`
 --
 
 INSERT INTO `anggota` (`id`, `kode`, `nama`, `jk`, `id_jurusan`, `hp`, `alamat`) VALUES
-(3, '2222', 'TRISENDI', 'L', 6, '00000', 'BOJONG NANGKA');
+(3, '2222', 'TRISENDI', 'L', 6, '00000', 'BOJONG NANGKA'),
+(4, '3', 'CANDA', 'p', 9, '089432764224', 'uyuy');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buku`
+-- Struktur dari tabel `buku`
 --
 
 CREATE TABLE `buku` (
@@ -61,7 +62,7 @@ CREATE TABLE `buku` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `buku`
+-- Dumping data untuk tabel `buku`
 --
 
 INSERT INTO `buku` (`id`, `kode`, `judul`, `penulis`, `penerbit`, `tahun_terbit`, `stok`) VALUES
@@ -70,8 +71,8 @@ INSERT INTO `buku` (`id`, `kode`, `judul`, `penulis`, `penerbit`, `tahun_terbit`
 -- --------------------------------------------------------
 
 --
--- Stand-in structure for view `joinanggota`
--- (See below for the actual view)
+-- Stand-in struktur untuk tampilan `joinanggota`
+-- (Lihat di bawah untuk tampilan aktual)
 --
 CREATE TABLE `joinanggota` (
 `id` int(9)
@@ -87,7 +88,7 @@ CREATE TABLE `joinanggota` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jurusan`
+-- Struktur dari tabel `jurusan`
 --
 
 CREATE TABLE `jurusan` (
@@ -96,7 +97,7 @@ CREATE TABLE `jurusan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `jurusan`
+-- Dumping data untuk tabel `jurusan`
 --
 
 INSERT INTO `jurusan` (`id`, `jurusan`) VALUES
@@ -109,22 +110,47 @@ INSERT INTO `jurusan` (`id`, `jurusan`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `peminjam`
+-- Struktur dari tabel `peminjam`
 --
 
 CREATE TABLE `peminjam` (
   `id` int(11) NOT NULL,
-  `tanggal_pinjam` date NOT NULL,
+  `tanggal_pinjam` datetime NOT NULL,
   `tanggal_kembali` date NOT NULL,
   `id_buku` int(11) NOT NULL,
   `id_anggota` int(11) NOT NULL,
   `id_petugas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `peminjam`
+--
+
+INSERT INTO `peminjam` (`id`, `tanggal_pinjam`, `tanggal_kembali`, `id_buku`, `id_anggota`, `id_petugas`) VALUES
+(1, '2023-04-04 10:24:37', '2023-04-11', 2, 4, 2);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengembalian`
+-- Stand-in struktur untuk tampilan `peminjaman`
+-- (Lihat di bawah untuk tampilan aktual)
+--
+CREATE TABLE `peminjaman` (
+`id` int(11)
+,`tanggal_pinjam` datetime
+,`tanggal_kembali` date
+,`id_buku` int(11)
+,`judul` varchar(100)
+,`id_anggota` int(11)
+,`nama` varchar(60)
+,`id_petugas` int(11)
+,`nama_petugas` varchar(50)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengembalian`
 --
 
 CREATE TABLE `pengembalian` (
@@ -139,29 +165,30 @@ CREATE TABLE `pengembalian` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `petugas`
+-- Struktur dari tabel `petugas`
 --
 
 CREATE TABLE `petugas` (
   `id` int(9) NOT NULL,
   `foto` varchar(100) NOT NULL,
-  `nama` varchar(50) NOT NULL,
+  `nama_petugas` varchar(50) NOT NULL,
   `jabatan` varchar(50) NOT NULL,
   `hp` varchar(13) NOT NULL,
   `alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `petugas`
+-- Dumping data untuk tabel `petugas`
 --
 
-INSERT INTO `petugas` (`id`, `foto`, `nama`, `jabatan`, `hp`, `alamat`) VALUES
-(2, 'Capture.PNG', 'ROSSA', 'PRESIDEN', '00000', 'ghgj');
+INSERT INTO `petugas` (`id`, `foto`, `nama_petugas`, `jabatan`, `hp`, `alamat`) VALUES
+(2, 'Capture.PNG', 'ROSSA', 'PRESIDEN', '00000', 'ghgj'),
+(3, 'u_connect.png', 'CANDA', 'GURU', '089432764224', 'HFJSDHFIO');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rak`
+-- Struktur dari tabel `rak`
 --
 
 CREATE TABLE `rak` (
@@ -174,7 +201,7 @@ CREATE TABLE `rak` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -184,7 +211,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`) VALUES
@@ -193,18 +220,27 @@ INSERT INTO `user` (`id`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure for view `joinanggota`
+-- Struktur untuk view `joinanggota`
 --
 DROP TABLE IF EXISTS `joinanggota`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `joinanggota`  AS SELECT `a`.`id` AS `id`, `a`.`kode` AS `kode`, `a`.`nama` AS `nama`, `a`.`jk` AS `jk`, `a`.`id_jurusan` AS `id_jurusan`, `b`.`jurusan` AS `jurusan`, `a`.`hp` AS `hp`, `a`.`alamat` AS `alamat` FROM (`anggota` `a` join `jurusan` `b` on(`a`.`id_jurusan` = `b`.`id`))  ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur untuk view `peminjaman`
+--
+DROP TABLE IF EXISTS `peminjaman`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `peminjaman`  AS SELECT `a`.`id` AS `id`, `a`.`tanggal_pinjam` AS `tanggal_pinjam`, `a`.`tanggal_kembali` AS `tanggal_kembali`, `a`.`id_buku` AS `id_buku`, `b`.`judul` AS `judul`, `a`.`id_anggota` AS `id_anggota`, `c`.`nama` AS `nama`, `a`.`id_petugas` AS `id_petugas`, `d`.`nama_petugas` AS `nama_petugas` FROM (((`peminjam` `a` join `buku` `b` on(`a`.`id_buku` = `b`.`id`)) join `anggota` `c` on(`a`.`id_anggota` = `c`.`id`)) join `petugas` `d` on(`a`.`id_petugas` = `d`.`id`))  ;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `anggota`
+-- Indeks untuk tabel `anggota`
 --
 ALTER TABLE `anggota`
   ADD PRIMARY KEY (`id`),
@@ -213,19 +249,19 @@ ALTER TABLE `anggota`
   ADD KEY `id_jurusan_3` (`id_jurusan`);
 
 --
--- Indexes for table `buku`
+-- Indeks untuk tabel `buku`
 --
 ALTER TABLE `buku`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `jurusan`
+-- Indeks untuk tabel `jurusan`
 --
 ALTER TABLE `jurusan`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `peminjam`
+-- Indeks untuk tabel `peminjam`
 --
 ALTER TABLE `peminjam`
   ADD PRIMARY KEY (`id`),
@@ -234,7 +270,7 @@ ALTER TABLE `peminjam`
   ADD KEY `id_petugas` (`id_petugas`);
 
 --
--- Indexes for table `pengembalian`
+-- Indeks untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
   ADD PRIMARY KEY (`id`),
@@ -243,88 +279,88 @@ ALTER TABLE `pengembalian`
   ADD KEY `id_anggota` (`id_anggota`);
 
 --
--- Indexes for table `petugas`
+-- Indeks untuk tabel `petugas`
 --
 ALTER TABLE `petugas`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `rak`
+-- Indeks untuk tabel `rak`
 --
 ALTER TABLE `rak`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_buku` (`id_buku`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `anggota`
+-- AUTO_INCREMENT untuk tabel `anggota`
 --
 ALTER TABLE `anggota`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `buku`
+-- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `jurusan`
+-- AUTO_INCREMENT untuk tabel `jurusan`
 --
 ALTER TABLE `jurusan`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `peminjam`
+-- AUTO_INCREMENT untuk tabel `peminjam`
 --
 ALTER TABLE `peminjam`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `pengembalian`
+-- AUTO_INCREMENT untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `petugas`
+-- AUTO_INCREMENT untuk tabel `petugas`
 --
 ALTER TABLE `petugas`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `rak`
+-- AUTO_INCREMENT untuk tabel `rak`
 --
 ALTER TABLE `rak`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `anggota`
+-- Ketidakleluasaan untuk tabel `anggota`
 --
 ALTER TABLE `anggota`
   ADD CONSTRAINT `anggota_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `peminjam`
+-- Ketidakleluasaan untuk tabel `peminjam`
 --
 ALTER TABLE `peminjam`
   ADD CONSTRAINT `peminjam_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -332,7 +368,7 @@ ALTER TABLE `peminjam`
   ADD CONSTRAINT `peminjam_ibfk_3` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `pengembalian`
+-- Ketidakleluasaan untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
   ADD CONSTRAINT `pengembalian_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -340,7 +376,7 @@ ALTER TABLE `pengembalian`
   ADD CONSTRAINT `pengembalian_ibfk_3` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `rak`
+-- Ketidakleluasaan untuk tabel `rak`
 --
 ALTER TABLE `rak`
   ADD CONSTRAINT `rak_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
